604e9b6b983183a969e5a2417bbdd4fed3dec198
Ha <nguyen.thi.ha-b@sun-asterisk.com>
2024-08-14 08:41:53 +0700

    create homepage

--- my_wibu_list/settings.py
+++ my_wibu_list/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wibu_catalog',
 ]
 
 MIDDLEWARE = [
@@ -147,3 +148,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'wibu_catalog/static',
+]
---
+++ wibu_catalog/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+def homepage(request):
+    return render(request, 'html/homepage.html')
