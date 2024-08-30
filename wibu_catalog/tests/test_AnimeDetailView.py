6e9b78233e9019fcfc29c3e010511a94a87cc317
Tran Ba Thanh B <tran.ba.thanh-b@sun-asterisk.com>
2024-08-30 14:12:30 +0700

    f test unit

--- wibu_catalog/tests/test_AnimeDetailView.py
+++ wibu_catalog/tests/test_AnimeDetailView.py
@@ -1,18 +1,6 @@
-"""
-Test info:
-    test_anime_detail_view_context_data: Tests if all context data is correctly
-        populated.
-    test_anime_detail_view_random_button_functionality: Tests if the random
-        button returns a valid Content instance.
-    test_anime_detail_view_pagination: Tests if pagination works correctly
-        for comments.
-    test_anime_detail_view_no_products: Tests behavior when there are no
-        products related to the content.
-    test_anime_detail_view_user_favorite_status: Tests the favorite status
-        functionality for a logged-in user.
-"""
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django.utils import timezone  # Add this import
 from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
 from ..views import AnimeDetailView
@@ -25,6 +13,7 @@ from wibu_catalog.models import (
     ScoreList,
     Comments,
     Product,
+    Notifications,  # Add this import if it's used
 )
 
 class AnimeDetailViewTests(TestCase):
@@ -35,14 +24,14 @@ class AnimeDetailViewTests(TestCase):
             username="UserOne",
             email="userone@example.com",
             password="password123",
-            registrationDate=timezone.now(),
+            registrationDate=timezone.now(),  # Fixed
         )
         self.user2 = Users.objects.create(
             uid=2,
             username="UserTwo",
             email="usertwo@example.com",
             password="password456",
-            registrationDate=timezone.now(),
+            registrationDate=timezone.now(),  # Fixed
         )
 
         # Create Content
@@ -102,14 +91,14 @@ class AnimeDetailViewTests(TestCase):
             uid=self.user1,
             cid=self.content1,
             content="Amazing anime!",
-            dateOfCmt=timezone.now(),
+            dateOfCmt=timezone.now(),  # Fixed
             likes=10,
         )
         self.comment2 = Comments.objects.create(
             uid=self.user2,
             cid=self.content2,
             content="Exciting manga!",
-            dateOfCmt=timezone.now(),
+            dateOfCmt=timezone.now(),  # Fixed
             likes=5,
         )
 
@@ -117,14 +106,14 @@ class AnimeDetailViewTests(TestCase):
         self.notification1 = Notifications.objects.create(
             uid=self.user1,
             message="You have a new message.",
-            date=timezone.now(),
+            date=timezone.now(),  # Fixed
             nType="message",
             isRead=False,
         )
         self.notification2 = Notifications.objects.create(
             uid=self.user2,
             message="Your order has been shipped.",
-            date=timezone.now(),
+            date=timezone.now(),  # Fixed
             nType="order",
             isRead=True,
         )
@@ -158,9 +147,9 @@ class AnimeDetailViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'html/anime_detail.html')
         self.assertContains(response, self.content1.name)
-        self.assertContains(response, self.comment.content)
-        self.assertContains(response, self.favorite.cid.name)
-        self.assertContains(response, '8')  # Check if the score is correctly displayed
+        self.assertContains(response, self.comment1.content)  # Fixed to comment1
+        self.assertContains(response, self.favorite1.cid.name)  # Fixed to favorite1
+        self.assertContains(response, '10')  # Fixed to match score_list1
         self.assertIsNotNone(response.context['userr'])  # Ensure user is in context
 
     def test_anime_detail_view_without_logged_in_user(self):
@@ -169,8 +158,8 @@ class AnimeDetailViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'html/anime_detail.html')
         self.assertContains(response, self.content1.name)
-        self.assertContains(response, self.comment.content)
-        self.assertNotContains(response, '8')  # No score should be shown for anonymous user
+        self.assertContains(response, self.comment1.content)  # Fixed to comment1
+        self.assertNotContains(response, '10')  # No score should be shown for anonymous user
         self.assertIsNone(response.context['userr'])  # No user in context
 
     def test_anime_detail_view_context_data(self):
@@ -180,7 +169,7 @@ class AnimeDetailViewTests(TestCase):
         context = response.context
         self.assertEqual(context["anime_detail"], self.content1)
         self.assertEqual(list(context["comments"]), list(Comments.objects.filter(cid=self.content1.cid)))
-        self.assertEqual(context["favorite"], self.favorite)
+        self.assertEqual(context["favorite"], self.favorite1)  # Fixed to favorite1
         self.assertIsNotNone(context["what_to_watch"])
         self.assertEqual(list(context["products"]), list(Product.objects.filter(cid=self.content1.cid)))
         self.assertIn("score_", context)
@@ -199,7 +188,7 @@ class AnimeDetailViewTests(TestCase):
                 cid=self.content1,
                 uid=self.user1,
                 content=f"Comment {i}",
-                dateOfCmt="2024-01-01"
+                dateOfCmt=timezone.now()  # Fixed
             )
 
         response = self.client.get(reverse('anime_detail', kwargs={'pk': self.content1.cid}), {'comments_page': 2})
@@ -219,4 +208,4 @@ class AnimeDetailViewTests(TestCase):
         session.save()
 
         response = self.client.get(reverse('anime_detail', kwargs={'pk': self.content1.cid}))
-        self.assertEqual(response.context['favorite'], self.favorite)  # Favorite should be present in context
+        self.assertEqual(response.context['favorite'], self.favorite1)  # Fixed to favorite1
